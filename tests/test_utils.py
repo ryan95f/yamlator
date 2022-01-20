976cbdea522c934db7c3f72aea27e479a6155434
Ryan95Z <Ryan95f@gmail.com>
2022-01-20 20:45:34 +0000

    renamed the tests file directory to files. Added a coveragerc file. Added tests for load_yamler_rulesets

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import unittest
 from yamler.utils import load_yaml_file
+from yamler.utils import load_yamler_ruleset
 
 
 class TestLoadYamlFile(unittest.TestCase):
@@ -10,9 +11,23 @@ class TestLoadYamlFile(unittest.TestCase):
             with self.assertRaises(ValueError):
                 load_yaml_file(filename)
 
-    def test_yaml_file_with_successful_read(self):
-        yaml_file = "tests/yaml/hello.yaml"
-        results = load_yaml_file(yaml_file)
+    def test_successfully_load_yaml_file(self):
+        yaml_file_path = "tests/files/hello.yaml"
+        results = load_yaml_file(yaml_file_path)
+        self.assertIsNotNone(results)
+
+
+class TestLoadYamlerRuleset(unittest.TestCase):
+    def test_load_yamler_ruleset_invalid_filename(self):
+        filenames = [None, ""]
+
+        for filename in filenames:
+            with self.assertRaises(ValueError):
+                load_yamler_ruleset(filename)
+
+    def test_successfully_load_yamler_file(self):
+        yamler_file_path = "tests/files/hello.yamler"
+        results = load_yamler_ruleset(yamler_file_path)
         self.assertIsNotNone(results)
 
 
