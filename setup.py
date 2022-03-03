041bd95c4d7a5565c373faff0a4116087684f425
Ryan95Z <Ryan95f@gmail.com>
2022-03-03 12:47:30 +0000

    created a very basic setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yamler',
+      version='0.1.0'
+      description='A',
+      author='Ryan Flynn',
+      packages=["lark==1.0.0", "PyYAML==6.0"])
