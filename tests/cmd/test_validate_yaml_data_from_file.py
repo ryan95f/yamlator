9c0e1cab950aa8178ea3df976e31b727ccad6735
Ryan95Z <Ryan95F@gmail.com>
2022-03-04 11:05:47 +0000

    started to create a test case for a valid run of validate yaml from file

--- tests/cmd/test_validate_yaml_data_from_file.py
+++ tests/cmd/test_validate_yaml_data_from_file.py
@@ -5,23 +5,26 @@ from collections import namedtuple
 from yamler.cmd import validate_yaml_data_from_file
 from yamler.exceptions import InvalidRulesetFilenameError
 
+VALID_YAML_DATA_FILE_PATH = "./tests/files/hello.yaml"
+VALID_RULESET_FILE_PATH = "./tests/files/hello.yamler"
+
 ValidateArgs = namedtuple('ValidateArgs', ['yaml_filepath', 'ruleset_filepath'])
 
 
 class TestValidateYamlDataFromFile(unittest.TestCase):
     @parameterized.expand([
-        ('none_yaml_path', ValidateArgs(None, "./tests/files/hello.yamler"), ValueError),
-        ('none_ruleset_path', ValidateArgs("./tests/files/hello.yaml", None), ValueError),
+        ('none_yaml_path', ValidateArgs(None, VALID_RULESET_FILE_PATH), ValueError),
+        ('none_ruleset_path', ValidateArgs(VALID_YAML_DATA_FILE_PATH, None), ValueError),
         ('yaml_data_file_not_found', ValidateArgs(
             'not_found.yaml',
-            "./tests/files/hello.yamler"
+            VALID_RULESET_FILE_PATH
         ), FileNotFoundError),
         ('ruleset_file_not_found', ValidateArgs(
-            "./tests/files/hello.yaml",
+            VALID_YAML_DATA_FILE_PATH,
             "not_found.yamler"
         ), FileNotFoundError),
         ('ruleset_invalid_file_extension', ValidateArgs(
-            "./tests/files/hello.yaml",
+            VALID_YAML_DATA_FILE_PATH,
             "./tests/files/hello.ruleset"
         ), InvalidRulesetFilenameError)
     ])
@@ -30,6 +33,13 @@ class TestValidateYamlDataFromFile(unittest.TestCase):
                                                             expected_exception: Exception):  # nopep8
         with self.assertRaises(expected_exception):
             validate_yaml_data_from_file(args.yaml_filepath, args.ruleset_filepath)
+
+    def test_validate_yaml_data_from_file_with_valid_data(self):
+        violations = validate_yaml_data_from_file(
+            yaml_filepath=VALID_YAML_DATA_FILE_PATH,
+            ruleset_filepath=VALID_RULESET_FILE_PATH
+        )
+        self.assertIsNotNone(violations)
 
 
 if __name__ == '__main__':
