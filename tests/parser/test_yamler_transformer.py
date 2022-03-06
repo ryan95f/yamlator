df847856fcc44162402b5a0ae039800607e09e6f
Ryan95Z <Ryan95f@gmail.com>
2022-03-06 14:11:21 +0000

    removed some duplicate data from the yamler transformer test

--- tests/parser/test_yamler_transformer.py
+++ tests/parser/test_yamler_transformer.py
@@ -12,47 +12,44 @@ Token = namedtuple('Token', ['value'])
 class TestYamlerTransformer(unittest.TestCase):
     def setUp(self):
         self.transformer = YamlerTransformer()
+        self.name_token = Token('message')
+        self.status_code_token = Token('StatusCode')
+
+        self.str_rtype = RuleType(type=SchemaTypes.STR)
+        self.ruleset_rules = [
+            Rule('name', RuleType(type=SchemaTypes.STR), True),
+            Rule('age', RuleType(type=SchemaTypes.INT), True),
+        ]
 
     def test_required_rule(self):
-        rtype = RuleType(type=SchemaTypes.STR)
-        name = Token('message')
-        tokens = (name, rtype)
+        tokens = (self.name_token, self.str_rtype)
 
         required_rule = self.transformer.required_rule(tokens)
-        self.assertEqual(name.value, required_rule.name)
+        self.assertEqual(self.name_token.value, required_rule.name)
         self.assertTrue(required_rule.is_required)
 
     def test_optional_rule(self):
-        rtype = RuleType(type=SchemaTypes.STR)
-        name = Token('message')
-        tokens = (name, rtype)
-
+        tokens = (self.name_token, self.str_rtype)
         optional_rule = self.transformer.optional_rule(tokens)
-        self.assertEqual(name.value, optional_rule.name)
+
+        self.assertEqual(self.name_token.value, optional_rule.name)
         self.assertFalse(optional_rule.is_required)
 
     def test_ruleset(self):
         name = Token('person')
-        rules = [
-            Rule('name', RuleType(type=SchemaTypes.STR), True),
-            Rule('age', RuleType(type=SchemaTypes.INT), True),
-        ]
-        tokens = (name, *rules)
-
+        tokens = (name, *self.ruleset_rules)
         ruleset = self.transformer.ruleset(tokens)
+
         self.assertEqual(name.value, ruleset.name)
-        self.assertEqual(len(rules), len(ruleset.rules))
+        self.assertEqual(len(self.ruleset_rules), len(ruleset.rules))
 
     def test_main_ruleset(self):
         expected_ruleset_name = 'main'
-        rules = [
-            Rule('name', RuleType(type=SchemaTypes.STR), True),
-            Rule('age', RuleType(type=SchemaTypes.INT), True),
-        ]
-        tokens = (*rules, )
+        tokens = (*self.ruleset_rules, )
         ruleset = self.transformer.main_ruleset(tokens)
+
         self.assertEqual(expected_ruleset_name, ruleset.name)
-        self.assertEqual(len(rules), len(ruleset.rules))
+        self.assertEqual(len(self.ruleset_rules), len(ruleset.rules))
 
     def test_start(self):
         # This will be zero since main is removed from the dict
@@ -73,10 +70,8 @@ class TestYamlerTransformer(unittest.TestCase):
         ruleset_items = self.transformer.start(instructions)
         rulesets = ruleset_items.get('rules')
         enums = ruleset_items.get('enums')
-        self.assertIsNotNone(ruleset_items.get('main'))
-        self.assertIsNotNone(rulesets)
-        self.assertIsNotNone(enums)
 
+        self.assertIsNotNone(ruleset_items.get('main'))
         self.assertEqual(expected_enum_count, len(enums))
         self.assertEqual(expected_ruleset_count, len(rulesets))
 
@@ -89,20 +84,22 @@ class TestYamlerTransformer(unittest.TestCase):
         self.assertEqual(SchemaTypes.INT, int_rule_type.type)
 
     def test_ruleset_type(self):
-        name = Token('message')
-        ruleset_type = self.transformer.ruleset_type((name, ))
+        ruleset_type = self.transformer.ruleset_type((self.name_token, ))
+
         self.assertEqual(SchemaTypes.RULESET, ruleset_type.type)
-        self.assertEqual(name.value, ruleset_type.lookup)
+        self.assertEqual(self.name_token.value, ruleset_type.lookup)
 
     def test_list_type(self):
-        tokens = (RuleType(type=SchemaTypes.STR), )
+        tokens = (self.str_rtype, )
         list_type = self.transformer.list_type(tokens)
+
         self.assertEqual(SchemaTypes.LIST, list_type.type)
         self.assertEqual(tokens[0], list_type.sub_type)
 
     def test_map_type(self):
-        tokens = (RuleType(type=SchemaTypes.STR), )
+        tokens = (self.str_rtype, )
         map_type = self.transformer.map_type(tokens)
+
         self.assertEqual(SchemaTypes.MAP, map_type.type)
         self.assertEqual(tokens[0], map_type.sub_type)
 
@@ -111,16 +108,18 @@ class TestYamlerTransformer(unittest.TestCase):
         self.assertEqual(SchemaTypes.ANY, any_type.type)
 
     def test_enum_type(self):
-        tokens = (Token('StatusCode'), )
-        enum_type = self.transformer.enum_type(tokens)
+        enum_type = self.transformer.enum_type((self.status_code_token, ))
+
         self.assertEqual(SchemaTypes.ENUM, enum_type.type)
-        self.assertEqual(tokens[0].value, enum_type.lookup)
+        self.assertEqual(self.status_code_token.value, enum_type.lookup)
 
     def test_enum_item(self):
         enum_name = Token('StatusCode')
         enum_value = Token('success')
+
         tokens = (enum_name, enum_value)
         enum_item = self.transformer.enum_item(tokens)
+
         self.assertEqual(enum_name.value, enum_item.name)
         self.assertEqual(enum_value.value, enum_item.value)
 
@@ -129,16 +128,16 @@ class TestYamlerTransformer(unittest.TestCase):
             EnumItem('SUCCESS', 'success'),
             EnumItem('ERR', 'error')
         ]
-        tokens = (Token('StatusCode'), *enum_items)
+
+        tokens = (self.status_code_token, *enum_items)
         enum = self.transformer.enum(tokens)
-        self.assertIsNotNone(enum)
-        self.assertEqual('StatusCode', enum.name)
+
+        self.assertEqual(self.status_code_token.value, enum.name)
         self.assertEqual(len(enum_items), len(enum.items))
 
     def test_type(self):
-        token = Token('str')
-        type_token = self.transformer.type((token, ))
-        self.assertEqual(token, type_token)
+        type_token = self.transformer.type((self.name_token, ))
+        self.assertEqual(self.name_token, type_token)
 
 
 if __name__ == '__main__':
