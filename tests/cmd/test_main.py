2fa447924785fc6b8406ec3053f05dc39810df73
Ryan95Z <Ryan95F@gmail.com>
2022-03-08 21:51:47 +0000

    removed logging capabilities from the main function

--- tests/cmd/test_main.py
+++ tests/cmd/test_main.py
@@ -9,9 +9,8 @@ from yamler.cmd import main, SUCCESS, ERR
 HELLO_YAML_FILE_PATH = './tests/files/hello.yaml'
 HELLO_RULESET_FILE_PATH = './tests/files/hello.yamler'
 INVALID_HELLO_YAML_FILE_PATH = './test/files/invalid_hello.yaml'
-NONE_LOG_LEVEL = 'NONE'
 
-ValidateArgs = namedtuple('ValidateArgs', ['file', 'ruleset_schema', 'log_level'])
+ValidateArgs = namedtuple('ValidateArgs', ['file', 'ruleset_schema'])
 
 
 class TestMain(unittest.TestCase):
@@ -19,38 +18,31 @@ class TestMain(unittest.TestCase):
     @parameterized.expand([
         ('with_yaml_matching_ruleset', ValidateArgs(
             HELLO_YAML_FILE_PATH,
-            HELLO_RULESET_FILE_PATH,
-            NONE_LOG_LEVEL
+            HELLO_RULESET_FILE_PATH
         ), SUCCESS),
         ('with_yaml_containing_ruleset_violations', ValidateArgs(
             INVALID_HELLO_YAML_FILE_PATH,
-            HELLO_RULESET_FILE_PATH,
-            NONE_LOG_LEVEL
+            HELLO_RULESET_FILE_PATH
         ), ERR),
         ('with_ruleset_file_not_found', ValidateArgs(
             HELLO_YAML_FILE_PATH,
-            '/test/files/not_found.yamler',
-            NONE_LOG_LEVEL
+            '/test/files/not_found.yamler'
         ), ERR),
         ('with_yaml_data_not_found', ValidateArgs(
             './tests/files/not_found.yaml',
-            HELLO_RULESET_FILE_PATH,
-            NONE_LOG_LEVEL
+            HELLO_RULESET_FILE_PATH
         ), ERR),
         ('with_empty_yaml_file_path', ValidateArgs(
             '',
-            HELLO_RULESET_FILE_PATH,
-            NONE_LOG_LEVEL
+            HELLO_RULESET_FILE_PATH
         ), ERR),
         ('with_empty_ruleset_path', ValidateArgs(
             HELLO_YAML_FILE_PATH,
-            '',
-            NONE_LOG_LEVEL
+            ''
         ), ERR),
         ('with_invalid_ruleset_extension', ValidateArgs(
             HELLO_YAML_FILE_PATH,
-            './test/files/hello.ruleset',
-            NONE_LOG_LEVEL
+            './test/files/hello.ruleset'
         ), ERR)
     ])
     @patch('argparse.ArgumentParser')
