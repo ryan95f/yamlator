3b429f9f1f8208cff689847afb392955a542d16a
Ryan95Z <Ryan95f@gmail.com>
2022-04-03 13:02:15 +0100

    fixed the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.1'
 PACKAGE_NAME = 'yamler'
 DESCRIPTION = 'Yamler is a CLI tool that allows a YAML file to be validated against a schema'  # nopep8
 
