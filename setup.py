2032520aa0887a9c704a39d5b5f6e05b0f15b8f0
Ryan Flynn <9076629+Ryan95Z@users.noreply.github.com>
2022-04-03 13:03:47 +0100

    Pypi setup (#19)
    * updated the setup.py file to include more classifiers
    
    * created a new workflow to upload to pypi

--- setup.py
+++ setup.py
@@ -5,12 +5,17 @@ import setuptools
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+VERSION = '0.0.1'
+PACKAGE_NAME = 'yamler'
+DESCRIPTION = 'Yamler is a CLI tool that allows a YAML file to be validated against a schema'  # nopep8
+
 setuptools.setup(
-    name='yamler',
-    version='0.1.0',
-    description='A tool to validate that a YAML file conforms to a given schema',
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/Ryan95Z/yamler',
     author='Ryan Flynn',
     packages=setuptools.find_packages(include=['yamler']),
     install_requires=[
@@ -19,12 +24,14 @@ setuptools.setup(
     ],
     python_requires='>=3.7',
     classifiers=[
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent'
     ],
     entry_points={
         'console_scripts': ['yamler=yamler.cmd:main']
