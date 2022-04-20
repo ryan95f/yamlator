e052708e4b0e5b4dab98137523ea7ff9dc464188
Ryan95Z <Ryan95f@gmail.com>
2022-04-20 19:08:05 +0100

    added additional test cases to the any type validator test case

--- tests/validators/test_any_type_validator.py
+++ tests/validators/test_any_type_validator.py
@@ -10,9 +10,15 @@ from src.validators import AnyTypeValidator
 class TestAnyTypeValidator(BaseValidatorTest):
 
     @parameterized.expand([
-        ('is_any_type', {'message': 'test'}, RuleType(type=SchemaTypes.ANY), 0),
+        ('is_any_type_with_dict', {'message': 'test'}, RuleType(type=SchemaTypes.ANY), 0),
+        ('is_any_type_with_list', [1, 2, 3, 4], RuleType(type=SchemaTypes.ANY), 0),
+        ('is_any_type_with_none', None, RuleType(type=SchemaTypes.ANY), 0),
+        ('is_any_type_with_str', 100, RuleType(type=SchemaTypes.ANY), 0),
         ('is_list_type', [0, 1, 2, 3, 4], RuleType(
-            type=list, sub_type=RuleType(type=SchemaTypes.INT)), 1),
+            type=SchemaTypes.LIST, sub_type=RuleType(type=SchemaTypes.INT)), 1),
+        ('is_ruleset_type', {'val': 42}, RuleType(
+            type=SchemaTypes.RULESET, lookup='value'), 1),
+        ('is_int_type', 100, RuleType(type=SchemaTypes.INT), 1)
     ])
     @patch('src.validators.Validator.validate')
     def test_any_type_validator(self, name: str, data: Data, rtype: RuleType,
